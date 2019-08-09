8860f2c4e6a14d0293a8fe88be60b60de472959d
Mayta Trigozo, G <g.mayta.trigozo@accenture.com>
2019-08-09 07:47:24 +0200

    move routes configuration from app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,39 +1,13 @@
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule } from '@angular/router';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { AppComponent } from './app.component';
 import { ServicesModule } from 'services/services.module';
+import { RoutesConfig } from './app.routes';
 import { environment } from '../environments/environment';
-
-const ROUTES: Routes = [
-  {
-    path: '',
-    loadChildren: () => import('./features/home/home.module').then(m => m.HomeModule)
-  },
-  {
-    path: 'teams',
-    loadChildren: () => import('./features/teams/teams.module').then(m => m.TeamsModule)
-  },
-  {
-    path: 'teams/:id',
-    loadChildren: () => import('./features/team/team.module').then(m => m.TeamModule)
-  },
-  {
-    path: 'standings',
-    loadChildren: () => import('./features/standings/standings.module').then(m => m.StandingsModule)
-  },
-  {
-    path: 'matches',
-    loadChildren: () => import('./features/matches/matches.module').then(m => m.MatchesModule)
-  },
-  {
-    path: 'favorites',
-    loadChildren: () => import('./features/favorites/favorites.module').then(m => m.FavoritesModule)
-  }
-];
 
 @NgModule({
   declarations: [
@@ -42,12 +16,13 @@ const ROUTES: Routes = [
   imports: [
     BrowserModule,
     ServicesModule,
-    RouterModule.forRoot(ROUTES),
+    RouterModule.forRoot(RoutesConfig),
     StoreModule.forRoot({}),
     EffectsModule.forRoot([]),
-    StoreDevtoolsModule.instrument()
+    StoreDevtoolsModule.instrument({
+      logOnly: environment.production
+    })
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
