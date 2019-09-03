95c5cdc39e8197003d9de0472c2692ce5fd94510
Mayta Trigozo, G <g.mayta.trigozo@accenture.com>
2019-09-03 11:22:50 +0200

    handle matches day

--- src/app/features/matches/matches.component.ts
+++ src/app/features/matches/matches.component.ts
@@ -10,13 +10,25 @@ import { MatchesState, LoadMatches } from './state';
   styleUrls: ['./matches.component.scss']
 })
 export class MatchesComponent implements OnInit {
-  matches$: Observable<Match[]>;
+  matches$: Observable<any>;
 
   constructor(private store: Store<MatchesState>) {
     this.store.dispatch(new LoadMatches);
   }
 
   ngOnInit() {
-    this.matches$ = this.store.select(({ matches }) => matches.data);
+    this.matches$ = this.store.select(({ matches }) => {
+      const key = 'matchday';
+      return matches.data.reduce((acc, value) => {
+        (acc[value[key]] = acc[value[key]] || []).push(value);
+        return acc;
+      }, {});
+    });
+  }
+
+  descOrder = (a: any, b: any) => {
+    if (a.key < b.key) {
+      return b.key;
+    }
   }
 }
